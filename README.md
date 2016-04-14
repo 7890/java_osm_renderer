cc15f08b022e6983c3ca8c6f1ca646042611b89a
Thomas Brand <tom@trellis.ch>
2016-04-15 00:00:57 +0200

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # java_osm_renderer
 render OSM tiles. initially copied from https://code.google.com/archive/p/simple-osm-render/
+
+```
+"Simple and fast multi threaded OSM tile renderer inspired by libmemphis. It is fully threaded and can draw 60 tiles or more per second on old laptops."
+
+this edition:
+
+-remove need for package com.objectplanet.image (import com.objectplanet.image.PngEncoder), replace with javax.imageio
+-reduce testdata set to "amsterdam.osm"
+-add servlet-api-3.1.jar
+-add build file (make.sh)
+-create javadoc
+
+```
---
+++ make.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+FULLPATH="`pwd`/$0"
+DIR=`dirname "$FULLPATH"`
+
+src="$DIR"/src
+build="$DIR"/_build
+archive="$DIR"/archive
+doc="$DIR"/doc
+
+jsource=1.6
+jtarget=1.6
+
+#linux / osx different mktemp call
+TMPFILE=`mktemp 2>/dev/null || mktemp -t /tmp`
+
+JAVAC="javac -source $jsource -target $jtarget -nowarn"
+
+NOW=`date +"%s"`
+
+#========================================================================
+checkAvail()
+{
+	which "$1" >/dev/null 2>&1
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "tool \"$1\" not found. please install"
+		exit 1
+	fi
+}
+
+#========================================================================
+compile_simple-osm-render()
+{
+	echo "building simple-osm-render (com.jetdrone.map.*)"
+	echo "==============================================="
+
+	echo "compiling files in $src/java/main to directory $build ..."
+
+	find "$src/java/main/" -name *.java > "$TMPFILE"
+
+	cp "$archive"/servlet-api-3.1.jar "$build"
+
+	$JAVAC -classpath "$build"/servlet-api-3.1.jar:"$build" -sourcepath "$src/java/main" -d "$build" @"$TMPFILE"
+}
+
+#========================================================================
+create_javadoc()
+{
+	echo "creating doc"
+	echo "============"
+
+	cd "$src/java/main"
+	javadoc -quiet -private -linksource -sourcetab 2 -d "$doc" \
+		-classpath .:"$build"/servlet-api-3.1.jar -sourcepath . \
+		com.jetdrone.map \
+		com.jetdrone.map.index \
+		com.jetdrone.map.mercator \
+		com.jetdrone.map.render \
+		com.jetdrone.map.render.backend \
+		com.jetdrone.map.rules \
+		com.jetdrone.map.rules.osm \
+		com.jetdrone.map.source \
+		com.jetdrone.map.source.osm
+	cd "$DIR"
+}
+
+for tool in java javac jar javadoc date; \
+	do checkAvail "$tool"; done
+
+mkdir -p "$build"
+rm -rf "$build"/*
+
+compile_simple-osm-render
+create_javadoc
+
+echo ""
+echo "done."
